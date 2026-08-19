b3794b3e4d684feb8a8eec200f696e8111573204
Jery Bn <jery.becerra@tecsup.edu.pe>
2026-08-19 15:25:39 -0500

    Initial commit

---
+++ app/src/main/java/com/Becerra/laboratorio00/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.Becerra.laboratorio00
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.Becerra.laboratorio00.ui.theme.Laboratorio00Theme
+import androidx.compose.foundation.layout.Column
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio00Theme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+
+        Text(
+            text = "¡Hola, soy $name!",
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = "Curso: Programación en Móviles"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio00Theme {
+        Greeting("Jery")
+    }
+}
